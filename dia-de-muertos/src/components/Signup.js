fd31aee06216907b69f79e6a35d65ef47c3a1dad
EmmaNem <emmabv1@gmail.com>
2018-09-09 18:03:18 -0700

    signup form

---
+++ dia-de-muertos/src/components/Signup.js
@@ -0,0 +1,103 @@
+import React, {Component} from "react";
+import axios from "axios";
+
+class Signup extends Component {
+    state = {
+        name: "",
+        email: "",
+        password: "",
+        confirm: ""
+    }
+
+    handleChange = event => {
+        this.setState({[event.target.name]: event.target.value})
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        axios.post("/api/users", {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password
+        })
+            .then(res => {console.log(res)})
+            .catch(err => {console.log(err)})
+    };
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <div>
+                        <div>
+                            <label>Name</label>
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                placeholder="Name"
+                                value={this.state.name}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <label>Email</label>
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                id="email"
+                                name="email"
+                                placeholder="Email"
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <label>Password</label>
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                id="password"
+                                name="password"
+                                placeholder="Password"
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <label>Confirm Password</label>
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                id="confirm"
+                                name="confirm"
+                                placeholder="Confirm Password"
+                                value={this.state.confirm}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <button
+                            type="submit"
+                            onClick={this.handleSubmit}
+                        >Sign Up</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+};
+
+export default Signup;
--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,19 @@ const User = require('../models/User');
 
 router.get('/api/users', (req, res) => {
     User.find()
-        .then(users => res.json(users))
+        .then(data => res.json(data))
+});
+
+router.post('/api/users', (req, res) => {
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    newUser.save((err, data) => {
+        if (err) return res.json(err)
+        res.json(data)
+    })
 });
 
 module.exports = router;
